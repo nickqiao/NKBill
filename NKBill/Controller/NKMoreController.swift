4e37663c66b133096aecb9b4ebbecb7346252ccb
NickQiao <393533945@qq.com>
2016-02-15 11:18:44 +0800

    change name

--- NKBill/Controller/NKMoreController.swift
+++ NKBill/Controller/NKMoreController.swift
@@ -24,34 +24,51 @@ class NKMoreController: UITableViewController {
         tableView.reloadData()
     }
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allowsNotification() ? 2 : 1
+        if section == 0 {
+            return allowsNotification() ? 2 : 1
+        }
+        return 1
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCellWithIdentifier("more")
-        if indexPath.row == 0 {
+        
+        if indexPath.section == 0 {
             
-            cell?.textLabel?.text = "接收新消息通知"
-            if allowsNotification() == true {
-                cell?.detailTextLabel?.text = "已开启"
-            }else {
-                cell?.detailTextLabel?.text = "已关闭"
+            if indexPath.row == 0 {
+                
+                cell?.textLabel?.text = "接收新消息通知"
+                if allowsNotification() == true {
+                    cell?.detailTextLabel?.text = "已开启"
+                }else {
+                    cell?.detailTextLabel?.text = "已关闭"
+                }
+                
+                return cell!
             }
             
+            cell?.textLabel?.text = "设置通知时间"
+            cell?.detailTextLabel?.text = ""
+            cell?.accessoryType = .DisclosureIndicator
             return cell!
         }
-        
-        cell?.textLabel?.text = "设置通知时间"
-        cell?.detailTextLabel?.text = ""
-        cell?.accessoryType = .DisclosureIndicator
+       
+        cell?.textLabel?.text = "欢迎留言QQ"
+        cell?.detailTextLabel?.text = "393533945"
         return cell!
         
     }
 
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "如果您要关闭或开启微信的消息通知,请在iPhone的\"设置\"-\"通知\"功能中,找到应用程序更改"
+        if section == 0 {
+            return "如果您要关闭或开启微信的消息通知,请在iPhone的\"设置\"-\"通知\"功能中,找到应用程序更改"
+        }
+        return nil
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
