8c39f61484211bbb77c37001eaefac95291b50af
NickQiao <393533945@qq.com>
2016-02-16 14:00:53 +0800

    first store

--- NKBill/AppDelegate.swift
+++ NKBill/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        print("\(NSHomeDirectory())")
+        //print("\(NSHomeDirectory())")
         // 设置界面颜色
         customAppearce()
         // 配置本地通知
